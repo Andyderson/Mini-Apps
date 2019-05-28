3fe5be84556fc9195b14f4899eee1379c7617d43
Anderson Tsang <andyderson@gmail.com>
2019-05-28 11:53:59 -0700

    digitalClock minute color

--- digitalclock/src/App.js
+++ digitalclock/src/App.js
@@ -4,7 +4,8 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: new Date()
+      time: new Date(),
+      even: true
     };
   }
 
@@ -23,11 +24,21 @@ class App extends React.Component {
   }
 
   render() {
+    let minutes = this.state.time.getMinutes();
+
+    if (minutes % 2 === 0) {
+      this.state.even = true;
+    } else {
+      this.state.even = false;
+    }
+
     return (
       <div className="parent">
         Digital Clock
         <div className="clock">
-          <span className="time">{this.state.time.toLocaleTimeString()}</span>
+          <span className={this.state.even ? "timeEven" : "time"}>
+            {this.state.time.toLocaleTimeString()}
+          </span>
         </div>
       </div>
     );
